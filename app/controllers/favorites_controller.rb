eb94cfd911d8c8bd2b65100968549a8a57a88060
manami young <manayou1125@outlook.jp>
2022-08-04 11:29:37 +0900

    プロフィールからお気に入り一覧画面の閲覧

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,9 +1,9 @@
 class FavoritesController < ApplicationController
 
   def index
-    @favorites = current_user.favorites_pictures
+    @favorites = current_user.favorite_pictures
   end
-  
+
   def create
     favorite = current_user.favorites.create(picture_id: params[:picture_id])
     redirect_to pictures_path, notice: "#{favorite.picture.user.name}さんのブログをお気に入り登録しました"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :pictures
   has_many :favorites, dependent: :destroy
+  has_many :favorite_pictures, through: :favorites, source: :picture
   validates :name, presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 }
   before_validation { email.downcase! }
