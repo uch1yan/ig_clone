6e80db35ec35040a382145440cf066a62014a0d9
manami young <manayou1125@outlook.jp>
2022-08-05 10:46:39 +0900

    heroku login

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show, :edit, :update]
   resources :favorites, only: [:index, :create, :destroy]
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  root to: 'users#new'
   resources :pictures do
     collection do
       post :confirm
